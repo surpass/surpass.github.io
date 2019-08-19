bc7dc9d00be90e57ebccb3fabbfa11c1b2748712
Frank Lee <lizc@tg-hd.com>
2019-08-19 17:01:01 +0800

    1

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 export HUOG_HOME=/data/devops/hugo/easyolap.cn
 
 sudo docker rm easyolap-hugo -f
-sudo rm -rf #(HUOG_HOME)/output/*
+sudo rm -rf $HUOG_HOME/output/*
 sudo docker run --env HUGO_BASEURL='https://easyolap.cn/' \
 --env HUGO_THEME=AllinOne --name "easyolap-hugo" \
 --publish-all \
---volume $(HUOG_HOME)/:/src --volume $(HUOG_HOME)/output:/output jojomi/hugo
+--volume $HUOG_HOME/:/src --volume $HUOG_HOME/output:/output jojomi/hugo
